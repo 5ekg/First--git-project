bd25e887ca0b2969965a7cf789b01ec403677335
5ekg <ghobadi.shobeir@yahoo.com>
2024-09-16 17:11:12 +0330

    Second exercise of the book

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 2. In the exercise section book asks to implement SVM and I changed the original code to make it. and the processing part took too much time almost several minutes. and the score
     relatively was high (68366)
+3. It also asks for using  RandomizedSearchCV: results for [2, 5, 10] n_iter: 47700 , 47600, 54000. 
 
---
+++ randomizedcv.py
@@ -0,0 +1,144 @@
+import os
+import numpy as np
+import tarfile
+import urllib.request
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+# os.makedirs(IMAGES_PATH, exist_ok=True)
+#
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+# def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+#     os.makedirs(housing_path, exist_ok=True)
+#     tgz_path = os.path.join(housing_path, "housing.tgz")
+#     urllib.request.urlretrieve(housing_url, tgz_path)
+#     housing_tgz = tarfile.open(tgz_path)
+#     housing_tgz.extractall(path=housing_path)
+#     housing_tgz.close()
+# fetch_housing_data()
+
+#reading the dataset into pandas
+
+import pandas as pd
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+housing = load_housing_data()
+# print(housing.head())
+housing.info()
+housing["ocean_proximity"].value_counts()
+housing.hist(bins=50, figsize=(20,15))
+plt.savefig('histogram.png')
+
+np.random.seed(42)
+
+# Divide by 1.5 to limit the number of income categories
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+# Label those above 5 as 5
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+from sklearn.model_selection import StratifiedShuffleSplit
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+print(housing["income_cat"].value_counts())
+for set in (strat_train_set, strat_test_set):
+    set.drop(["income_cat"], axis=1, inplace=True)
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+housing_num = housing.drop("ocean_proximity", axis=1)
+
+#imputer
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+
+#custom Transformers
+from sklearn.base import BaseEstimator, TransformerMixin
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+from sklearn.base import BaseEstimator, TransformerMixin
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names].values
+# automation by pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+# Modify your pipeline to use OneHotEncoder instead of LabelBinarizer
+num_pipeline = Pipeline([
+    ('selector', DataFrameSelector(num_attribs)),
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+])
+
+# Use OneHotEncoder for categorical data
+cat_pipeline = Pipeline([
+    ('selector', DataFrameSelector(cat_attribs)),
+    ('one_hot_encoder', OneHotEncoder()),  # Replace LabelBinarizer with OneHotEncoder
+])
+
+from sklearn.compose import ColumnTransformer
+
+# Combine both pipelines using ColumnTransformer
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", cat_pipeline, cat_attribs),
+])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+param_grid = [{'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},{'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},]
+forest_reg = RandomForestRegressor()
+grid_search = RandomizedSearchCV(forest_reg, param_grid, n_iter= 2, cv=5,
+scoring='neg_mean_squared_error')
+grid_search.fit(housing_prepared, housing_labels)
+final_model = grid_search.best_estimator_
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+X_test_prepared = full_pipeline.transform(X_test)
+final_predictions = final_model.predict(X_test_prepared)
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse) # => evaluates to 48,209.6
+print(final_rmse)
